4b2eac7dd5dda0d1259ef5304d89c01ddaa6ae4e
Kyoscripter <140461084+Kyoscripter@users.noreply.github.com>
2025-02-23 18:24:08 +0300

    ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 -- //{"MainColor":"1a1919","AccentColor":"ff00e5","OutlineColor":"000000","BackgroundColor":"141414","FontColor":"ffffff"}
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Lonera'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1a1919","AccentColor":"ff00e5","BackgroundColor":"141414","OutlineColor":"000000"}') },
+		['Lonera'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"ff00ed","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
 		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
